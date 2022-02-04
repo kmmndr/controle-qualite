f0143896c46f4ecc69b06430f0eec06bd5137573
Thomas Kienlen <t.kienlen@captive.fr>
2022-02-04 16:48:10 +0100

    Add first files

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM alpine:3.15
+RUN sed -i -e 's|^\(.*\)v3.15/main|\1edge/testing\n&|' /etc/apk/repositories
+
+WORKDIR /srv/app
+
+RUN apk add --no-cache \
+      coreutils \
+      git \
+      make \
+      ruby \
+      ruby-sassc \
+      nodejs \
+      npm
+
+RUN apk add --no-cache binutils pandoc \
+ && strip /usr/bin/pandoc \
+ && apk del binutils
+
+RUN gem install --no-document bundler \
+      bundler-audit:0.9.0.1 \
+      brakeman:5.2.1 \
+      rubocop:1.25.1 rubocop-performance rubocop-rails \
+      slim_lint:0.22.1 \
+      pandoc-ruby
+
+RUN npm install -g \
+      eslint@7.29.0 \
+      prettier@2.3.2 \
+      standard-prettier@1.0.1 \
+      standard@16.0.4 \
+      stylelint@13.13.1
+
+COPY --chmod=+x controle-qualite.mk /usr/local/bin
---
+++ controle-qualite.mk
@@ -0,0 +1,87 @@
+#!/usr/bin/env -S make -f
+
+help: ##- Print available commands
+	@echo
+	@echo "Contrôle qualité:"
+	@sed -e '/#\{2\}-/!d; s/\\$$//; s/:[^#\t]*/:/; s/#\{2\}-*//; s/^/  /' $(MAKEFILE_LIST)
+
+c: check-all ##- Alias for check-all
+check-all: check-ruby check-css check-js ##- check ruby, css, js
+
+s: self-check ##- Alias for self-check
+self-check: self-check-ruby self-check-node ##- Check tools versions
+
+GEM_LIST=rubocop|slim_lint|bundler-audit|brakeman
+
+self-check-ruby: ##- Check ruby tools versions
+	@echo
+	@echo "Checking gem versions"
+	@echo
+	@echo "Installed versions"
+	@gem list | awk '/(${GEM_LIST}) / { print "- " $$0 }'
+	@echo
+	@echo "Checking for updates ..."
+	@gem outdated | awk 'BEGIN { return_code = 0} /(${GEM_LIST}) / { print "- " $$0; return_code = 1 } END { exit return_code }'
+
+###
+## Ruby
+#
+
+check-ruby: ##- Check ruby code quality
+check-ruby: rubocop slim-lint bundle-audit brakeman
+
+rubocop:
+	@echo "Checking ruby code quality"
+	rubocop -f simple
+
+slim-lint:
+	@echo "Checking slim code quality"
+	slim-lint .
+
+bundle-audit:
+	@echo "Checking for vulnerabilities in bundled gems"
+	bundle-audit check --update
+
+brakeman:
+	@echo "Checking for vulnerabilities ruby code"
+	brakeman --run-all-checks --no-progress --no-pager --quiet --ignore-config .brakeman.ignore
+
+###
+## Node
+#
+
+self-check-node: ##- Check node tools versions
+	@echo
+	@echo "Checking node packages versions"
+	@echo
+	@echo "Installed versions"
+	@npm ls -g
+	@echo
+	@echo "Checking for updates ..."
+	@npm outdated -g
+
+###
+## CSS
+#
+
+check-css: ##- Check css code quality
+check-css: stylelint
+
+stylelint:
+	@echo "Checking scss code"
+	stylelint app/**/*.scss
+
+###
+## JS
+#
+
+check-js: ##- Check js code quality
+check-js: eslint prettier
+
+eslint:
+	@echo "Checking js syntax"
+	eslint app/**/*.js
+
+prettier:
+	@echo "Checking js syntax"
+	prettier --check app
